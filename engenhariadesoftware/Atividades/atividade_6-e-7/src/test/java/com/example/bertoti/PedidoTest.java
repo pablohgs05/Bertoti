5f4053e86e776ad0e67b3a959dabc85c84d690e3
Pablo Henrique <pablo.silva61@fatec.sp.gov.br>
2024-05-24 20:39:40 -0300

    Revert "mudando cliente"
    This reverts commit d7d3be227a2da4b95d66ab998d66af8524e6b6fa.

--- engenhariadesoftware/Atividades/atividade_6-e-7/src/test/java/com/example/bertoti/PedidoTest.java
+++ engenhariadesoftware/Atividades/atividade_6-e-7/src/test/java/com/example/bertoti/PedidoTest.java
@@ -7,7 +7,7 @@ public class PedidoTest {
 
     @Test
     public void testAdicionarItem() {
-        Cliente cliente = new Cliente("1", "Pablo", "pablohenrique@gmail.com");
+        Cliente cliente = new Cliente("1", "João", "joao@example.com");
         Pedido pedido = new Pedido("1", cliente);
         ItemPedido item = new ItemPedido("1", "P1", 2, 50.0);
 
@@ -19,7 +19,7 @@ public class PedidoTest {
 
     @Test
     public void testCalcularTotalPedido() {
-        Cliente cliente = new Cliente("1", "Pablo", "pablohenrique@gmail.com");
+        Cliente cliente = new Cliente("1", "João", "joao@example.com");
         Pedido pedido = new Pedido("1", cliente);
         ItemPedido item1 = new ItemPedido("1", "P1", 2, 50.0);
         ItemPedido item2 = new ItemPedido("2", "P2", 1, 100.0);
